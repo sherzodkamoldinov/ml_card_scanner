01aee9079b9036f42f4d3958308e115623f133f2
sherzodkamoldinov <sherzodkamoldinov1999@gmail.com>
2025-07-17 16:05:27 +0500

    fix(scanner): reduce memory usage in computeImage, isolate-safe and debug-free

--- lib/src/utils/scanner_processor.dart
+++ lib/src/utils/scanner_processor.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 import 'dart:isolate';
+import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:ml_card_scanner/src/model/card_info.dart';
 import 'package:ml_card_scanner/src/parser/parser_algorithm.dart';
@@ -12,20 +14,21 @@ import 'package:ml_card_scanner/src/utils/stream_debouncer.dart';
 
 class ScannerProcessor {
   static const _kDebugOutputCooldownMillis = 5000;
+
   final bool _usePreprocessingFilters;
   final bool _debugOutputFilteredImage;
   final TextRecognizer _recognizer = TextRecognizer(
     script: TextRecognitionScript.latin,
   );
+
   StreamController<Uint8List>? _debugImageStreamController;
 
-  // 🔐 Защита от перегрузки памяти
   bool _isProcessing = false;
 
   ScannerProcessor({
     bool usePreprocessingFilters = false,
     bool debugOutputFilteredImage = false,
-  })  : _usePreprocessingFilters = debugOutputFilteredImage,
+  })  : _usePreprocessingFilters = usePreprocessingFilters,
         _debugOutputFilteredImage = debugOutputFilteredImage {
     if (_debugOutputFilteredImage) {
       _debugImageStreamController = StreamController<Uint8List>.broadcast();
@@ -48,62 +51,7 @@ class ScannerProcessor {
     _isProcessing = true;
 
     try {
-      late InputImage inputImage;
-
-      if (!_usePreprocessingFilters) {
-        final format = InputImageFormatValue.fromRawValue(image.format.raw);
-        final bytes = Uint8List.fromList(
-          image.planes.fold<List<int>>(
-            [],
-                (acc, plane) => acc..addAll(plane.bytes),
-          ),
-        );
-        inputImage = InputImage.fromBytes(
-          bytes: bytes,
-          metadata: InputImageMetadata(
-            size: Size(image.width.toDouble(), image.height.toDouble()),
-            rotation: rotation,
-            format: format ?? InputImageFormat.yuv420,
-            bytesPerRow: image.planes.first.bytesPerRow,
-          ),
-        );
-      } else {
-        final rawFormat = image.format.raw;
-        final rawRotation = rotation.rawValue;
-        final Uint8List bytes = Uint8List.fromList(
-          image.planes.fold<List<int>>(
-            [],
-                (acc, plane) => acc..addAll(plane.bytes),
-          ),
-        );
-
-        ReceivePort? receivePort;
-        if (_debugOutputFilteredImage) {
-          receivePort = ReceivePort();
-          receivePort.listen(
-                (message) {
-              if (message is Uint8List &&
-                  _debugImageStreamController != null &&
-                  !_debugImageStreamController!.isClosed) {
-                _debugImageStreamController?.add(message);
-              }
-            },
-          );
-        }
-
-        inputImage = await createInputImageInIsolate(
-          rawBytes: bytes,
-          width: image.width,
-          height: image.height,
-          rawRotation: rawRotation,
-          rawFormat: rawFormat,
-          bytesPerRow: image.planes.first.bytesPerRow,
-          debugSendPort: receivePort?.sendPort,
-        );
-
-        receivePort?.close();
-      }
-
+      final inputImage = await _createInputImage(image, rotation);
       final recognizedText = await _recognizer.processImage(inputImage);
       final parsedCard = await parseAlgorithm.parse(recognizedText);
       return parsedCard;
@@ -112,6 +60,56 @@ class ScannerProcessor {
       return null;
     } finally {
       _isProcessing = false;
+    }
+  }
+
+  Future<InputImage> _createInputImage(
+      CameraImage image,
+      InputImageRotation rotation,
+      ) async {
+    final format = InputImageFormatValue.fromRawValue(image.format.raw);
+    final bytes = Uint8List.fromList(
+      image.planes.fold<List<int>>(
+        [],
+            (acc, plane) => acc..addAll(plane.bytes),
+      ),
+    );
+
+    if (!_usePreprocessingFilters) {
+      return InputImage.fromBytes(
+        bytes: bytes,
+        metadata: InputImageMetadata(
+          size: Size(image.width.toDouble(), image.height.toDouble()),
+          rotation: rotation,
+          format: format ?? InputImageFormat.yuv420,
+          bytesPerRow: image.planes.first.bytesPerRow,
+        ),
+      );
+    } else {
+      ReceivePort? receivePort;
+      if (_debugOutputFilteredImage) {
+        receivePort = ReceivePort();
+        receivePort.listen((message) {
+          if (message is Uint8List &&
+              _debugImageStreamController != null &&
+              !_debugImageStreamController!.isClosed) {
+            _debugImageStreamController?.add(message);
+          }
+        });
+      }
+
+      final inputImage = await createInputImageInIsolate(
+        rawBytes: bytes,
+        width: image.width,
+        height: image.height,
+        rawRotation: rotation.rawValue,
+        rawFormat: image.format.raw,
+        bytesPerRow: image.planes.first.bytesPerRow,
+        debugSendPort: receivePort?.sendPort,
+      );
+
+      receivePort?.close();
+      return inputImage;
     }
   }
 
